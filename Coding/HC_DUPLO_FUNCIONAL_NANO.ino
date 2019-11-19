fe9f44f8c4fbc1d92003fb0153611685a1757ea1
cavedo95 <juninho.cavedo@gmail.com>
2019-11-19 19:04:46 -0300

    Update HC_DUPLO_FUNCIONAL_NANO.ino

--- Coding/HC_DUPLO_FUNCIONAL_NANO.ino
+++ Coding/HC_DUPLO_FUNCIONAL_NANO.ino
@@ -1,23 +1,22 @@
+// Código de configuração da Bengala inteligente
+ 
 #include <HCSR04.h>
 
-#define INTERVALO_LEITURA 200 //(ms)
-
-//conexão dos pinos para o sensor ultrasonico e buzzer
+//conexão dos pinos para os sensores ultrassônicos e buzzer
 #define PIN_TRIGGER1   5
 #define PIN_ECHO1      4
-#define PIN_TRIGGER   3
-#define PIN_ECHO      2
+#define PIN_TRIGGER2   3
+#define PIN_ECHO2      2
 #define PIN_BUZZER      10
-#define PIN_BUZZER1     7
 
-UltraSonicDistanceSensor distanceSensor(5, 4);  // Initialize sensor that uses digital pins 13 and 12.
-UltraSonicDistanceSensor distanceSensor2(3, 2);
-int d1, d2;
+UltraSonicDistanceSensor distanceSensor(PIN_TRIGGER1, PIN_ECHO1);  // Inicializa o sensor ultrassônico 1 nos pinos 4 e 5
+UltraSonicDistanceSensor distanceSensor2(PIN_TRIGGER2, PIN_ECHO2); // Inicializa o sensor ultrassônico 2 nos pinos 2 e 3
+int d1, d2;     //Distâncias dos senssores 1 e 2 respectivamente
 char t='b';
-char r='a';
+
 void setup () {
-    Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
-    pinMode(PIN_BUZZER,     OUTPUT); 
+    Serial.begin(9600);  //Serial iniciado em 9600 baud
+    pinMode(PIN_BUZZER,     OUTPUT);    //pino digital 10 configurado como saída
 }
 
 void loop () {
