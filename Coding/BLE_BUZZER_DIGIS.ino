878e23bac1cd7bddf24e5b1cd010bd01d70184d5
cavedo95 <juninho.cavedo@gmail.com>
2019-11-19 18:58:45 -0300

    Update BLE_BUZZER_DIGIS.ino

--- Coding/BLE_BUZZER_DIGIS.ino
+++ Coding/BLE_BUZZER_DIGIS.ino
@@ -1,33 +1,33 @@
-//Configurar módulo BLE com comandos AT
-//Alimenta dados do BLE para a serial do Arduino e vice versa
+/*Código da Pulseira BLE usando uma Digispark
+Ambas bibliotecas foram adicionadas do repositório oficial da Digispark no GitHub.
+Na raiz Arduino/libraries --> git clone https://github.com/digistump/DigistumpArduino.git
+*/
+
 #include <SoftSerial.h>
 #include <TinyPinChange.h>
  
-#define SOFT_RX 3
+#define SOFT_RX 3       
 #define SOFT_TX 2
 #define PIN_VIBRA     1
 
+SoftSerial BLE_serial(2, 3); //rxPin = 2 , txPin = 3
 
-SoftSerial BLE_serial(2, 3); //rxPin = 3 , txPin = 4
 void setup()
 {
- pinMode(PIN_VIBRA,     OUTPUT); 
- BLE_serial.begin(9600); // Terminal BLE 
+ pinMode(PIN_VIBRA,     OUTPUT);  //Define o pino digital como saída.
+ BLE_serial.begin(9600); // Terminal BLE configurado em 9600 baud
 }
 void loop()
 {
- 
- while (BLE_serial.available() > 0) {
-    char inByte = BLE_serial.read();
-    Serial.print(inByte);
+  while (BLE_serial.available() > 0) {   //Laço que mantém a transmissão pelo BLE 1010 ativa 
+    char inByte = BLE_serial.read();     //Coloca a informação recebida por bluetooth em uma variável tipo char
 
-  if(inByte=='b'){
+  if(inByte=='b'){                       //Define as condições de vibração. No caso, quando recebe o char 'b' vibra por 50 ms e depois desliga.
   digitalWrite(PIN_VIBRA, HIGH);
   delay(50);
   digitalWrite(PIN_VIBRA, LOW);
   delay(50);
-  }
-  
+                 }
   }
 
 }
